21f7b48f4bd8116f34d20820b398c71ecba6b739
houhuiting <35065957+houhuiting@users.noreply.github.com>
2022-11-08 09:51:25 +0800

    dist/deb: support make deb packages (#115)
    Signed-off-by: Huiting Hou <houhuiting@pku.stu.edu.cn>

--- dist/deb/build.sh
+++ dist/deb/build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
-PROJECT_DIR=$(cd ../../..; pwd)
+PROJECT_DIR=$(cd ../..; pwd)
 DEBBUILD_DIR=$(mktemp -u /tmp/debbuild.XXXX)
 SCRIPT_DIR=$(pwd)
 PACKAGE=rats-tls
-PROJECT=inclavare-containers
 VERSION=$(cd ../..; cat ./VERSION)
 RELEASE_TARBALL=$DEBBUILD_DIR/v$VERSION.tar.gz
-RELEASE_TARBALL_URL=https://github.com/alibaba/inclavare-containers/archive/v$VERSION.tar.gz
 TARBALL_NAME=$PACKAGE\_$VERSION.orig.tar.gz
 DEB_BUILD_FOLDER=$DEBBUILD_DIR/$PACKAGE-$VERSION
 
 # create and rename the tarball
 mkdir -p $DEBBUILD_DIR
 if [ ! -f "$RELEASE_TARBALL" ]; then
-  wget -P $DEBBUILD_DIR $RELEASE_TARBALL_URL
+        cp -r $PROJECT_DIR $DEB_BUILD_FOLDER
 fi
-tar zxfP $DEBBUILD_DIR/v$VERSION.tar.gz -C $DEBBUILD_DIR
-mv $DEBBUILD_DIR/$PROJECT-$VERSION $DEBBUILD_DIR/$PACKAGE-$VERSION
 cd $DEBBUILD_DIR && tar zcfP $TARBALL_NAME $PACKAGE-$VERSION
 
 if [ -z "$SGX_SDK" ]; then
@@ -30,9 +25,32 @@ if [ ! -d "$SGX_SDK" ]; then
         exit 1
 fi
 
-# build_deb_package
-cp -rf  $SCRIPT_DIR/debian $DEB_BUILD_FOLDER
-cd $DEB_BUILD_FOLDER
-DEB_CFLAGS_SET="-std=gnu11 -fPIC" DEB_CXXFLAGS_SET="-std=c++11 -fPIC" DEB_LDFLAGS_SET="-fPIC" dpkg-buildpackage -us -uc
-cp $DEBBUILD_DIR/*.*deb $PROJECT_DIR
+# Use multiple modes
+BUILD_MODE[0]=host
+BUILD_MODE[1]=tdx
+BUILD_MODE[2]=occlum
+BUILD_MODE[3]=sgx
+
+# Generate deb packages by looping through multiple modes
+for BUILD_MODE in "${BUILD_MODE[@]}"; do
+        echo "Start build deb package with mode $BUILD_MODE"
+        rm -rf $DEB_BUILD_FOLDER/src
+        cp -r $PROJECT_DIR/src $DEB_BUILD_FOLDER/src
+        cp -rf  $SCRIPT_DIR/debian $DEB_BUILD_FOLDER
+        cd $DEB_BUILD_FOLDER
+	sed 's/Package: rats-tls/Package: rats-tls-'$BUILD_MODE'/g' debian/control.in > debian/control 
+        sed 's/cmake -DBUILD_SAMPLES=on -H. -Bbuild/cmake -DRATS_TLS_BUILD_MODE="'$BUILD_MODE'" -DBUILD_SAMPLES=on -H. -Bbuild/g' debian/rules.in > debian/rules
+        sed -i 's/rats-tls-host/rats-tls-'$BUILD_MODE'/g' debian/rules
+        if [ $BUILD_MODE == "sgx" ]; then
+                sed -i '/dh_strip --exclude=rats-tls-server/c\\tdh_strip --exclude=rats-tls-server --exclude=rats-tls-client --exclude=sgx_stub_enclave.signed.so --exclude=librats_tls.a --exclude=librats_tls_u.a --exclude=librtls_edl_t.a --exclude=libtls_wrapper*.a --exclude=libcrypto_wrapper*.a --exclude=libattester*.a --exclude=libverifier*.a' debian/rules
+                dpkg-buildpackage -us -uc
+        else
+                DEB_CFLAGS_SET="-std=gnu11 -fPIC" DEB_CXXFLAGS_SET="-std=c++11 -fPIC" DEB_LDFLAGS_SET="-fPIC" dpkg-buildpackage -us -uc
+        fi
+        cp $DEBBUILD_DIR/*.*.deb $PROJECT_DIR
+        rm -rf $DEB_BUILD_FOLDER/build
+        rm -rf $DEB_BUILD_FOLDER/debian
+        echo "Successfully build deb package with mode $BUILD_MODE"
+done
+
 rm -rf $DEBBUILD_DIR
