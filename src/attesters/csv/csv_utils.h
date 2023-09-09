06e8a288663ee866bbef31711db9df553b72e602
hanliyang <hanliyang@hygon.cn>
2023-09-09 20:44:11 +0000

    rats-tls: csv: validate the hmac of PEK and ChipId in attester
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- src/attesters/csv/csv_utils.h
+++ src/attesters/csv/csv_utils.h
@@ -8,3 +8,5 @@
 
 void gen_random_bytes(void *buf, size_t len);
 int sm3_hash(const unsigned char *data, size_t len, unsigned char *hash, size_t expected_hash_len);
+int sm3_hmac(const char *key, size_t key_len, const unsigned char *data, size_t data_len,
+	     unsigned char *hmac, size_t expected_hmac_len);
